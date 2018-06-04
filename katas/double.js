4a4d89a3cb23f5daa3ca39d0ffb69b6a11e987d0
Joel Palmer <joelpalmer@gmail.com>
2018-06-04 06:14:23 -0500

    add double

---
+++ katas/double.js
@@ -0,0 +1,7 @@
+const double = (array) => {
+    // Use array.map() to return a new array with each value twice
+    // as large as the corresponding value in the passed in array.
+    return array.map((e)=> {
+        return e*2;
+    })
+}
