04948368816430f1eee456b42ccb2f0b4046b475
Joel Palmer <joelpalmer@gmail.com>
2018-06-06 07:43:20 -0500

    Correct high word score

--- katas/high.js
+++ katas/high.js
@@ -1,38 +1,26 @@
-/* 
-6 kyu
-Highest Scoring Word
+const high = (x) => {
+    const words = x.split(" ");
+    let highestScore = 0;
+    let highestScoringWord = "";
 
-Given a string of words, you need to find the highest scoring word.
-
-Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
-
-You need to return the highest scoring word as a string.
-
-If two words score the same, return the word that appears earliest in the original string.
-
-All letters will be lowercase and all inputs will be valid.
-
-*/
-
-//high('man i need a taxi up to ubud'), => 'taxi'
-//create an array of object with letter and score
-const high = (x)=> {
-    const letters = x.split(" ").join("").split("");
-    const scores = letters.map((l, i) => {
-        return {
-            letter: l,
-            score: i+1
+    words.map((word) => {
+        let score = wordScore(word);
+        if (score > highestScore) {
+            highestScore = score;
+            highestScoringWord = word;
         }
     });
 
-    const words = x.split(" ");
-    const wordScores = words.map((w) => {
-        const scores = w.split("");
-        scores;
-    })
-    return scores;
-
+    return highestScoringWord;
 }
 
-const l = high('man i need a taxi up to ubud');
-l;
+const wordScore = (word) => {
+    const alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+    let score = 0;
+    word.split("").map((letter) => {
+        score += alphabet.indexOf(letter) + 1;
+    });
+    return score;
+}
+
+console.log(high("man i need a taxi up to ubud")); //, 'taxi');
--- katas/twosum.js
+++ katas/twosum.js
@@ -1,7 +1,7 @@
 /*
     Given a list of numbers, return whether any two sums to k.
 
-    Given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
+    Given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
     Bonus: Doing it one pass
 */
